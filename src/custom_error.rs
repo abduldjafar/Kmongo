89f0b7dde30492b0dc18e27e3cf206743863d16a
Abdul Haris Djafar <abdul.djafar@quipper.com>
2023-09-05 20:32:51 +0700

    remove unused vars

--- src/custom_error.rs
+++ src/custom_error.rs
@@ -5,10 +5,6 @@ use tokio::task::JoinError;
 use std::error::Error;
 
 
-pub const ERROR_DATABASE_CONNECTION: &str = "getting error connection to database";
-pub const ERROR_GET_DATA_FROM_DB: &str = "getting error when collect data from database";
-
-
 #[derive(Debug)]
 pub struct CustomError{
     pub message: String,
